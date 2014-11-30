9fa4a7a4581862087fe637be30023322066d6c3a
Lukas Woodtli <woodtli.lukas@gmail.com>
2014-11-30 11:10:05 +0100

    Work on emacs lisp tutorial

--- casting-spels-emacs.el
+++ casting-spels-emacs.el
@@ -2,6 +2,7 @@
 ;; M-x eval-buffer for evaluation complete buffer
 
 ;; If you did something wrong and Emacs starts showing error messages, you'll want to use the command M-x top-level
+
 (setq eval-expression-print-length nil)
 
 (require 'cl)
@@ -19,7 +20,7 @@
                    (downstairs stairway living-room))))
 
 
-(setq objects-locations '((whiskey-bottle living-room)
+(setq object-locations '((whiskey-bottle living-room)
                           (bucket living-room)
                           (chain garden)
                           (frog gatden)))
@@ -27,4 +28,27 @@
 (setq location 'living-room)
 
 
-;; http://www.lisperati.com/casting-spels-emacs/html/casting-spels-emacs-12.html
+(defun describe-location (location map)
+  (second (assoc location map)))
+
+
+;(message (describe-location 'living-room map))
+
+(defun describe-path (path)
+  `(there is a ,(second path) going ,(first path) from here -))
+
+;(message (describe-path '(west door garden)))
+
+(defun describe-paths (location map)
+    (apply #'append (mapcar #'describe-path (cddr (assoc location map)))))
+
+
+;(message (describe-paths 'living-room map))
+
+
+(defun is-at (obj loc obj-loc)
+  (eq (second (assoc obj obj-loc)) loc))
+
+;(message (is-at 'whiskey-bottle 'living-room object-locations))
+
+;; http://www.lisperati.com/casting-spels-emacs/html/casting-spels-emacs-19.html
